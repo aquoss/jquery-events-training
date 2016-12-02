c5d8f79bb36e52cc35b0f130fd3fbaa41bc2a5d8
aquoss <amberquoss@gmail.com>
2016-12-01 17:50:13 -0800

    added event listener and function to log clicks

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  $('span').on('click', function() {
+    var phrase = $(this).text() + " at " + Date.now();
+    $('ul').append('<li>'+phrase+'</li>');
+  })
 })
