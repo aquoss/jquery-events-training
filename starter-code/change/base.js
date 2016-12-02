bbcdc19651061d67c81a99c508f33ff268d4d978
aquoss <amberquoss@gmail.com>
2016-12-01 17:29:34 -0800

    added event listener and function to add the first two inputs

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,11 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
   $("#time").text( Date.now() );
-
+  $('.add').change(function() {
+    var a = $('#right').val();
+    var b = $('#left').val();
+    var total = Number(a) + Number(b);
+    $('#total').val(total);
+  })
 })
