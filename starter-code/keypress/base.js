cc70503408af69bcf3db442936e17502d6967bea
aquoss <amberquoss@gmail.com>
2016-12-01 18:37:31 -0800

    created the function to run the stopwatch

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,16 @@
 console.log("Sanity Check: JS is working!");
+var start;
 
 $(document).ready(function(){
-
+  $(window).keypress(function(e){
+    if(e.keyCode == 32) {
+      if (start) {
+        var end = Date.now();
+        var total = (end - start)/1000;
+        $("#total-time").text(total);
+      } else {
+        start = Date.now();
+      }
+    };
+  })
 })
