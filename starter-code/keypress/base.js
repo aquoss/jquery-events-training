36961077d63e28043da5048bf6c56f7af167b43d
aquoss <amberquoss@gmail.com>
2016-12-01 19:20:53 -0800

    completed the stretch to get the stopwatch to run live

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,16 +1,26 @@
 console.log("Sanity Check: JS is working!");
 var start;
+pressCount = 0;
 
 $(document).ready(function(){
   $(window).keypress(function(e){
     if(e.keyCode == 32) {
       if (start) {
+        pressCount +=1;
         var end = Date.now();
-        var total = (end - start)/1000;
+        total = (end - start)/1000;
         $("#total-time").text(total);
       } else {
         start = Date.now();
+        var interval = window.setInterval(counter, 1);
       }
-    };
+    }
   })
 })
+
+function counter() {
+  if (pressCount<1) {
+    var total = (Date.now() - start)/1000;
+    $("#total-time").text(total);
+  }
+}
